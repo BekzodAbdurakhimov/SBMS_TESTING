a04d72364ab9d2ed70f82a3eb58e5b674d63d558
Bekzod Abdurakhimov <bekzodabdurakhimov29@gmail.com>
2024-10-09 21:34:01 +0500

    added funcs to run_all_tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,5 +1,11 @@
 from user_data import get_and_save_user_data
 from test_rate_plan import test_rate_plan
+from test_serv_activate import test_serv_activate
+from test_serv_deactivate import test_serv_deactivate
+from test_sms_pack_activate import test_pack_activate
+from test_sms_pack_deactivate import test_pack_deactivate
+from test_internet_pack_activate import test_internet_pack_activate
+from test_internet_pack_deactivate import test_internet_pack_deactivate
 from selenium import webdriver
 
 def run_test_rate_plan():
@@ -13,6 +19,12 @@ def run_test_rate_plan():
     # Вызов функции теста
     try:
         test_rate_plan(driver)
+        test_serv_activate(driver)
+        test_serv_deactivate(driver)
+        test_pack_activate(driver)
+        test_pack_deactivate(driver)
+        test_internet_pack_activate(driver)
+        test_internet_pack_deactivate(driver)
     finally:
         driver.quit()
 

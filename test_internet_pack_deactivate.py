153ea2ba6d309a389e24e495032ce5a0702d9795
BekzodAbdurakhimov <blessed209b@gmail.com>
2024-10-14 22:19:43 +0500

    test_internet_p_da v1.2

--- test_internet_pack_deactivate.py
+++ test_internet_pack_deactivate.py
@@ -38,46 +38,46 @@ def test_internet_pack_deactivate(driver):
 
     log_step(' ======== Подключение Услуги ========')
 
-    # Проверка кнопки
-    detail_button_check = wait.until(
-        EC.visibility_of_element_located((By.ID, "details-button"))
-    )
-    detail_button_check.click()
-
-    go_to_link = wait.until(
-        EC.visibility_of_element_located((By.ID, "proceed-link"))
-    )
-    go_to_link.click()
-    login_text_locator = (By.CSS_SELECTOR, '.login-caption > span')
-    login_locator_check = wait.until(EC.visibility_of_element_located(login_text_locator))
-    assert login_locator_check.is_displayed(), "Не найден локатор входа"
-
-    login_input_locator = (By.CSS_SELECTOR, "input.sbms-textbox[name='user'][type='text']")
-    password_input_locator = (By.CSS_SELECTOR, "input.sbms-textbox[name='password'][type='password']")
-    enter_btn_locator = (By.CSS_SELECTOR, "button.sbms-button-ex")
-
-    # Ожидаем, пока поле ввода логина будет доступно для ввода
-    login_input_area = wait.until(EC.element_to_be_clickable(login_input_locator))
-    login_input_area.send_keys(LOGIN)
-
-    # Ожидаем, пока поле ввода пароля будет доступно для ввода
-    password_input_area = wait.until(EC.element_to_be_clickable(password_input_locator))
-    password_input_area.send_keys(PASSWORD)
-
-    # Ожидаем, пока кнопка "Войти" станет кликабельной
-    enter_btn = wait.until(EC.element_to_be_clickable(enter_btn_locator))
-    enter_btn.click()
-
-    log_step('Вход в SBMS выполнен')
-
-    # Ждать пока страничка не загрузиться полностью
-    wait.until(
-        lambda driver: "overflow: hidden; direction: ltr;" in driver.find_element(By.TAG_NAME,
-                                                                                  "body").get_attribute(
-            "style"))
-
-    # Вход в витрину
-
+    # # Проверка кнопки
+    # detail_button_check = wait.until(
+    #     EC.visibility_of_element_located((By.ID, "details-button"))
+    # )
+    # detail_button_check.click()
+    #
+    # go_to_link = wait.until(
+    #     EC.visibility_of_element_located((By.ID, "proceed-link"))
+    # )
+    # go_to_link.click()
+    # login_text_locator = (By.CSS_SELECTOR, '.login-caption > span')
+    # login_locator_check = wait.until(EC.visibility_of_element_located(login_text_locator))
+    # assert login_locator_check.is_displayed(), "Не найден локатор входа"
+    #
+    # login_input_locator = (By.CSS_SELECTOR, "input.sbms-textbox[name='user'][type='text']")
+    # password_input_locator = (By.CSS_SELECTOR, "input.sbms-textbox[name='password'][type='password']")
+    # enter_btn_locator = (By.CSS_SELECTOR, "button.sbms-button-ex")
+    #
+    # # Ожидаем, пока поле ввода логина будет доступно для ввода
+    # login_input_area = wait.until(EC.element_to_be_clickable(login_input_locator))
+    # login_input_area.send_keys(LOGIN)
+    #
+    # # Ожидаем, пока поле ввода пароля будет доступно для ввода
+    # password_input_area = wait.until(EC.element_to_be_clickable(password_input_locator))
+    # password_input_area.send_keys(PASSWORD)
+    #
+    # # Ожидаем, пока кнопка "Войти" станет кликабельной
+    # enter_btn = wait.until(EC.element_to_be_clickable(enter_btn_locator))
+    # enter_btn.click()
+    #
+    # log_step('Вход в SBMS выполнен')
+    #
+    # # Ждать пока страничка не загрузиться полностью
+    # wait.until(
+    #     lambda driver: "overflow: hidden; direction: ltr;" in driver.find_element(By.TAG_NAME,
+    #                                                                               "body").get_attribute(
+    #         "style"))
+    #
+    # # Вход в витрину
+    #
     # Локаторы для элементов меню
     cabs_locator = (By.XPATH, '//a[@class="menu__a-vertical" and text()="Витрины"]')
     subs_cabinet_locator = (By.XPATH, '//a[@class="menu__a-vertical" and text()="Витрина абонента"]')

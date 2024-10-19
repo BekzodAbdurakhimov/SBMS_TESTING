ad905648bfbba95ed0c021c07b083aea36e024d0
BekzodAbdurakhimov <blessed209b@gmail.com>
2024-10-19 23:24:05 +0300

    bugfix v1.3 input line clear func added

--- test_internet_pack_deactivate.py
+++ test_internet_pack_deactivate.py
@@ -159,12 +159,16 @@ def test_internet_pack_deactivate(driver):
 
     serv_search_input_clear_locator = (By.XPATH,
                                                   "//input[@ng-model='grdPacks.filter.name' and contains(@class, 'inp-text')]")
+    # serv_search_input_clear = wait.until(EC.element_to_be_clickable(serv_search_input_clear_locator))
+    # clear_input_action = ActionChains(driver)
+    # clear_input_action.double_click(serv_search_input_clear).perform()
+    # time.sleep(2)
+    # serv_search_input_clear.send_keys(Keys.BACKSPACE)
+    # time.sleep(2)
     serv_search_input_clear = wait.until(EC.element_to_be_clickable(serv_search_input_clear_locator))
-    clear_input_action = ActionChains(driver)
-    clear_input_action.double_click(serv_search_input_clear).perform()
-    time.sleep(2)
-    serv_search_input_clear.send_keys(Keys.BACKSPACE)
-    time.sleep(2)
+    serv_search_input_clear.send_keys(Keys.CONTROL + "A") # Для Mac Keys.COMMAND
+    serv_search_input_clear.send_keys(Keys.DELETE)
+    log_step('Очищен поле поиска Интернет Пакета')
 
     serv_search_input_locator = (By.XPATH,
                                             "//input[@ng-model='grdPacks.filter.name' and contains(@class, 'inp-text')]")
